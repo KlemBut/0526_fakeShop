ca508a9d73397ac91d1760e75ae9de1677a2a749
KlemBut <klemis94@gmail.com>
2022-05-29 16:56:10 +0300

    adding types

--- main.ts
+++ main.ts
@@ -1,103 +1,106 @@
-let mainWrapper = document.createElement('div') as HTMLElement;
-mainWrapper.classList.add("mainWrapper")
-document.querySelector(`body`)?.append(mainWrapper)
+let mainWrapper = document.createElement("div") as HTMLElement;
+mainWrapper.classList.add("mainWrapper");
+document.querySelector(`body`)?.append(mainWrapper);
 
 interface ProductsInterface {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
-    quantity: number
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  quantity: number;
 }
-let cart: ProductsInterface [] = []
-let products: ProductsInterface[] = []
-async function getProducts () {
-    let productsResponse = await fetch(`https://fakestoreapi.com/products`);
-    let productsData: ProductsInterface[] = await productsResponse.json();
-    return productsData
-}
-async function renderCards () {
-products = await getProducts()
+let cart: ProductsInterface[] = [];
+let products: ProductsInterface[] = [];
 
-products.map((product: ProductsInterface) => {
+async function getProducts(): Promise<ProductsInterface[]> {
+  let productsResponse = await fetch(`https://fakestoreapi.com/products`);
+  let productsData: ProductsInterface[] = await productsResponse.json();
+  return productsData;
+}
+async function renderCards() {
+  products = await getProducts();
+
+  products.map((product: ProductsInterface) => {
     let productItem = document.createElement(`div`) as HTMLElement;
-    let addToCart = document.createElement(`input`)
-    let image = document.createElement(`div`) as HTMLElement
+    let addToCart = document.createElement(`input`) as HTMLButtonElement;
+    let image = document.createElement(`div`) as HTMLElement;
     let textArea = document.createElement(`div`) as HTMLElement;
     let priceNButton = document.createElement(`div`) as HTMLElement;
-    let title = document.createElement(`h4`) as HTMLElement
-    let price = document.createElement(`h4`) as HTMLElement
-    let description = document.createElement(`p`) as HTMLElement
-    let category = document.createElement(`span`) as HTMLElement
-
+    let title = document.createElement(`h4`) as HTMLElement;
+    let price = document.createElement(`h4`) as HTMLElement;
+    let description = document.createElement(`p`) as HTMLElement;
+    let category = document.createElement(`span`) as HTMLElement;
+//@ts-ignore
     image.style = ` background-image: url(${product.image});
-                    `   
-    productItem.classList.add(`productWrapper`)
-    priceNButton.classList.add(`priceNButton`)
-    image.classList.add(`imgs`)
+                    `;
+    productItem.classList.add(`productWrapper`);
+    priceNButton.classList.add(`priceNButton`);
+    image.classList.add(`imgs`);
     title.textContent = product.title;
     price.textContent = `${product.price}$`;
     category.textContent = product.category;
     description.textContent = product.description;
     addToCart.setAttribute(`type`, `button`);
-    addToCart.setAttribute(`value`, `Add to Cart`)
-    addToCart.setAttribute(`id`, `${product.id}`)
-    textArea.classList.add(`textItem`)
-
+    addToCart.setAttribute(`value`, `Add to Cart`);
+    addToCart.setAttribute(`id`, `${product.id}`);
+    textArea.classList.add(`textItem`);
 
     mainWrapper.append(productItem);
     productItem.prepend(image);
-    priceNButton.append(price, addToCart)
+    priceNButton.append(price, addToCart);
     textArea.append(title, priceNButton, category, description);
-    productItem.append(textArea)
+    productItem.append(textArea);
 
-    addToCart.addEventListener(`click`, e => {
-        addToCRT(e)
-        console.log(cart);
-    })
-})
+    addToCart.addEventListener(`click`, (e) => {
+      addToCRT(e);
+      console.log(cart);
+    });
+  });
 }
-renderCards()
+renderCards();
 
-function addToCRT ( e:any) {
-    const id = Number(e.target.id)
-    const current:ProductsInterface = products.find(x => x.id === id)
-    const inCart = cart.find(x => x.id === id)
+function addToCRT(e: any): void {
+  const id: number = Number(e.target.id);
+  const current: ProductsInterface = products.find((x) => x.id === id);
+  const inCart: ProductsInterface | undefined  = cart.find((x) => x.id === id);
 
-    console.log(inCart);
-    if (inCart) {
-        const index  = cart.findIndex(x => x.id === id)
-        cart[index].quantity++
-    }else {
-        current.quantity = 1;
-        cart.push(current)
-    }
+  if (inCart) {
+    const index: number = cart.findIndex((x) => x.id === id);
+    cart[index].quantity++;
+  } else {
+    current.quantity = 1;
+    cart.push(current);
+  }
 
-    countCart()
-    pricer()
-    toLocalStorage()
-}
-
-function countCart () {
-    let quantityCounter = 0
-    cart.map((x:ProductsInterface) => {
-        quantityCounter += x.quantity
-    })
-    //@ts-ignore
-    document.querySelector(`.cart`)?.textContent = `Items in cart ${quantityCounter}`
+  countCart();
+  pricer();
+  toLocalStorage();
 }
 
-function pricer () {
-    let totalPrice = 0
-    cart.map((x:ProductsInterface) => {
-        totalPrice += x.price * x.quantity
-    })
-    //@ts-ignore
-    document.querySelector(`.price`)?.textContent = `Total price ${totalPrice.toFixed(2)} $`
+function countCart(): void {
+  let quantityCounter: number = 0;
+  cart.map((x: ProductsInterface) => {
+    quantityCounter += x.quantity;
+  });
+  //@ts-ignore
+  document.querySelector(
+    `.cart`
+  )?.textContent = `Items in cart ${quantityCounter}`;
 }
-function toLocalStorage () {
-    const cartJson = JSON.stringify(cart)
-    localStorage.setItem(`shopCart`, `${cartJson}`)
+
+function pricer(): void {
+  let totalPrice: number = 0;
+  cart.map((x: ProductsInterface) => {
+    totalPrice += x.price * x.quantity;
+  });
+  //@ts-ignore
+  document.querySelector(
+    `.price`
+  )?.textContent = `Total price ${totalPrice.toFixed(2)} $`;
+}
+function toLocalStorage(): void {
+  const cartJson: string = JSON.stringify(cart);
+  localStorage.setItem(`shopCart`, `${cartJson}`);
 }
--- shoppingCart.ts
+++ shoppingCart.ts
@@ -1,92 +1,88 @@
-let mainWrap = document.createElement('div') as HTMLElement;
-mainWrap.classList.add("mainWrapper")
-document.querySelector(`body`)?.append(mainWrap)
+let mainWrap = document.createElement("div") as HTMLElement;
+mainWrap.classList.add("mainWrapper");
+document.querySelector(`body`)?.append(mainWrap);
 
-// interface parsedCartItems 
+// interface parsedCartItems
 console.log(localStorage.getItem("shopCart"));
-const cartData: string = localStorage.getItem("shopCart")
-const parsedCart: ProductsInterface [] = JSON.parse(cartData)
+const cartData: string = localStorage.getItem("shopCart");
+const parsedCart: ProductsInterface[] = JSON.parse(cartData);
 let priceElement = document.querySelector(`.price`) as HTMLElement;
-let totalPrice: number = 0
+let totalPrice: number = 0;
 console.log(parsedCart);
 
 parsedCart.map((product: ProductsInterface) => {
-    let productItem = document.createElement(`div`) as HTMLElement;
-    let image = document.createElement(`div`) as HTMLElement
-    let textArea = document.createElement(`div`) as HTMLElement;
-    let priceNQuant = document.createElement(`div`) as HTMLElement;
-    let addButton = document.createElement(`input`) as HTMLElement
-    let removeButton = document.createElement(`input`) as HTMLElement
-    let title = document.createElement(`h4`) as HTMLElement
-    let price = document.createElement(`h4`) as HTMLElement
-    let quantity = document.createElement(`span`) as HTMLElement
+  let productItem = document.createElement(`div`) as HTMLElement;
+  let image = document.createElement(`div`) as HTMLElement;
+  let textArea = document.createElement(`div`) as HTMLElement;
+  let priceNQuant = document.createElement(`div`) as HTMLElement;
+  let addButton = document.createElement(`input`) as HTMLElement;
+  let removeButton = document.createElement(`input`) as HTMLElement;
+  let title = document.createElement(`h4`) as HTMLElement;
+  let price = document.createElement(`h4`) as HTMLElement;
+  let quantity = document.createElement(`span`) as HTMLElement;
+  //@ts-ignore
+  image.style = ` background-image: url(${product.image});
+                    `;
+  productItem.classList.add(`productWrapper`);
+  priceNQuant.classList.add(`priceNQuant`);
+  image.classList.add(`imgs`);
+  title.textContent = product.title;
+  price.textContent = `${product.price * product.quantity}$`;
+  quantity.textContent = `Quantity: ${product.quantity}`;
+  addButton.setAttribute(`type`, `button`);
+  addButton.setAttribute(`value`, `+`);
+  removeButton.setAttribute(`type`, `button`);
+  removeButton.setAttribute(`value`, `-`);
+  textArea.classList.add(`textItem`);
 
-    image.style = ` background-image: url(${product.image});
-                    `   
-    productItem.classList.add(`productWrapper`)
-    priceNQuant.classList.add(`priceNQuant`)
-    image.classList.add(`imgs`)
-    title.textContent = product.title;
-    price.textContent = `${product.price*product.quantity}$`;
+  mainWrap.append(productItem);
+  productItem.prepend(image);
+  priceNQuant.append(price, removeButton, addButton);
+  textArea.append(title, priceNQuant, quantity);
+  productItem.append(textArea);
+
+  addButton.addEventListener(`click`, (e) => {
+    product.quantity++;
+    price.textContent = `${product.price * product.quantity}$`;
     quantity.textContent = `Quantity: ${product.quantity}`;
-    addButton.setAttribute(`type`, `button`)
-    addButton.setAttribute(`value`, `+`)
-    removeButton.setAttribute(`type`, `button`)
-    removeButton.setAttribute(`value`, `-`)
-    textArea.classList.add(`textItem`)
+    quantityToLocalStorage();
+    renderTotalPrice();
+  });
+  removeButton.addEventListener(`click`, (e) => {
+    if (product.quantity > 1) {
+      product.quantity--;
+      price.textContent = `${product.price * product.quantity}$`;
+      quantity.textContent = `Quantity: ${product.quantity}`;
+      console.log(parsedCart);
+      quantityToLocalStorage();
+      renderTotalPrice();
+    } else {
+      product.quantity--;
+      productItem.remove();
+      localStorageRemove(product.id);
+      renderTotalPrice();
+    }
+  });
+});
+renderTotalPrice();
 
-
-    mainWrap.append(productItem);
-    productItem.prepend(image);
-    priceNQuant.append(price, removeButton, addButton)
-    textArea.append(title, priceNQuant, quantity, );
-    productItem.append(textArea)
-
-    addButton.addEventListener(`click`, e => {
-        product.quantity++
-        price.textContent = `${product.price*product.quantity}$`;
-        quantity.textContent = `Quantity: ${product.quantity}`;
-        quantityToLocalStorage ()
-        renderTotalPrice()
-    })
-    removeButton.addEventListener(`click`, e => {
-        if(product.quantity > 1){
-            product.quantity--
-            price.textContent = `${product.price*product.quantity}$`;
-            quantity.textContent = `Quantity: ${product.quantity}`;
-            console.log(parsedCart);
-            quantityToLocalStorage ()
-            renderTotalPrice()
-        }   else {
-            product.quantity--
-            productItem.remove()
-            localStorageRemove(product.id)
-            renderTotalPrice()
-        }
-        
-    })
-})
-renderTotalPrice()
-
-function localStorageRemove (productId: number) {
-    const index  = parsedCart.findIndex(x => x.id === productId)
-    console.log(parsedCart[index])
-    parsedCart.splice(index, 1)
-    console.log(parsedCart)
-     const cartJson = JSON.stringify(parsedCart)
-    localStorage.setItem(`shopCart`, `${cartJson}`)
+function localStorageRemove(productId: number): void {
+  const index: number = parsedCart.findIndex((x) => x.id === productId);
+  console.log(parsedCart[index]);
+  parsedCart.splice(index, 1);
+  console.log(parsedCart);
+  const cartJson: string = JSON.stringify(parsedCart);
+  localStorage.setItem(`shopCart`, `${cartJson}`);
 }
-function quantityToLocalStorage () {
-    const cartJson = JSON.stringify(parsedCart)
-    localStorage.setItem(`shopCart`, `${cartJson}`)
+function quantityToLocalStorage(): void {
+  const cartJson: string = JSON.stringify(parsedCart);
+  localStorage.setItem(`shopCart`, `${cartJson}`);
 }
 
-function renderTotalPrice(){
-
-    totalPrice = 0
-    parsedCart.map(product => {
-        totalPrice += product.quantity*product.price
-    })
-    priceElement.textContent = `Total price: ${totalPrice}`
+function renderTotalPrice(): void {
+  totalPrice = 0;
+  parsedCart.map((product) => {
+    totalPrice += product.quantity * product.price;
+  });
+  priceElement.textContent = `Total price: ${totalPrice}`;
 }
-
